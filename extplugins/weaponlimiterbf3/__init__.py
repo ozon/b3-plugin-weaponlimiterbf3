b1ba022a5a6aacacc0accbdc3eb97d235190dce4
Harry Gabriel <rootdesign@gmail.com>
2013-12-09 03:22:40 +0100

    add support for BF4 game

--- extplugins/weaponlimiterbf3/__init__.py
+++ extplugins/weaponlimiterbf3/__init__.py
@@ -18,23 +18,42 @@
 
 import b3
 import b3.events
-import b3.plugin
+from b3.plugin import Plugin
 from b3.parsers.frostbite2.protocol import CommandFailedError
 from pluginconf import PluginConfig
-from weapondef import WEAPON_NAMES_BY_ID
+from weapondef import WEAPON_NAMES_BY_ID, BF4_WEAPON_NAMES_BY_ID
 from b3.parsers.bf3 import MAP_NAME_BY_ID, GAME_MODES_NAMES, MAP_ID_BY_NAME, GAME_MODES_BY_MAP_ID
 
-__version__ = '0.9.1'
+from b3.parsers.bf4 import MAP_NAME_BY_ID as BF4_MAP_NAME_BY_ID
+from b3.parsers.bf4 import GAME_MODES_NAMES as BF4_GAME_MODES_NAMES
+from b3.parsers.bf4 import MAP_ID_BY_NAME as BF4_MAP_ID_BY_NAME
+from b3.parsers.bf4 import GAME_MODES_BY_MAP_ID as BF4_GAME_MODES_BY_MAP_ID
+
+
+__version__ = '0.9.2'
 __author__ = 'ozon'
 
 
-class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
+class Weaponlimiterbf3Plugin(Plugin):
+
+    MAP_NAME_BY_ID = None
+    GAME_MODES_NAMES = None
+    MAP_ID_BY_NAME = None
+    GAME_MODES_BY_MAP_ID = None
+    WEAPON_NAMES_BY_ID = None
     _adminPlugin = None
     _wpl_is_active = None
     _messages = {}
-    _default_messages = {}
+    _default_messages = {
+        'servermessage': 'This game uses weapons limits. Use !weaponlimits to show forbidden weapons.',
+        'weaponlimiter_enabled': 'WeaponLimiter for that round activated!',
+        'weaponlimiter_disabled': 'Weaponlimiter disabled! All Weapons allowed.',
+        'warn_message': 'You used a forbidden weapon! ^7!forbidden_weapons show the list of forbidden weapons',
+        'forbidden_message': 'Forbidden Weapons are: %s',
+        'allowed_message': 'Allowed Weapons: %s',
+        'extra_warning_message': 'You have %(victim)s killed with a %(weapon)s. This weapon is forbidden!',
+    }
     _plugin_config = None
-
     # general settings
     _settings = {}
     _default_settings = {
@@ -54,14 +73,8 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
     }
     # wpl dont work on:
     _disable_on_gamemode = ['GunMaster0']
-
     _mapconfig = {}
-    _mapconfig_template = {
-        'weapons': list(WEAPON_NAMES_BY_ID),
-        'gametype': list(GAME_MODES_NAMES),
-        'mode': 'blacklist'
-    }
-
+    _mapconfig_template = {}
     _old_servermessage = None
 
     def onLoadConfig(self):
@@ -90,9 +103,21 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
 
     def onStartup(self):
         """ Initialize plugin settings """
-        # check if bf3 game
-        if self.console.game.gameName != 'bf3':
-            self.error('This plugin only works with Battlefield3 (bf3).')
+        # check if bf3/bf4 game and load constants
+        if self.console.game.gameName == 'bf3':
+            self.MAP_NAME_BY_ID = MAP_NAME_BY_ID
+            self.GAME_MODES_NAMES = GAME_MODES_NAMES
+            self.MAP_ID_BY_NAME = MAP_ID_BY_NAME
+            self.GAME_MODES_BY_MAP_ID = GAME_MODES_BY_MAP_ID
+            self.WEAPON_NAMES_BY_ID = WEAPON_NAMES_BY_ID
+        elif self.console.game.gameName == 'bf4':
+            self.MAP_NAME_BY_ID = BF4_MAP_NAME_BY_ID
+            self.GAME_MODES_NAMES = BF4_GAME_MODES_NAMES
+            self.MAP_ID_BY_NAME = BF4_MAP_ID_BY_NAME
+            self.GAME_MODES_BY_MAP_ID = BF4_GAME_MODES_BY_MAP_ID
+            self.WEAPON_NAMES_BY_ID = BF4_WEAPON_NAMES_BY_ID
+        else:
+            self.error('This plugin only works with Battlefield 3 (bf3) or Battlefield 4 (bf4).')
             return False
         # try to load admin plugin
         self._adminPlugin = self.console.getPlugin('admin')
@@ -108,6 +133,12 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
         self.registerEvent(b3.events.EVT_GAME_WARMUP)
         self.registerEvent(b3.events.EVT_GAME_ROUND_END)
         self.registerEvent(b3.events.EVT_GAME_MAP_CHANGE)
+
+        self._mapconfig_template = {
+            'weapons': list(self.WEAPON_NAMES_BY_ID),
+            'gametype': list(self.GAME_MODES_NAMES),
+            'mode': 'blacklist'
+        }
 
     def onEvent(self, event):
         """ Handle CLIENT_KILL and GAME_ROUND_START events """
@@ -154,7 +185,7 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
     # punish player
     def _punish_player(self, event, data=None, client=None):
         """ Punish player """
-        weapon = WEAPON_NAMES_BY_ID[data.data[1]]['name']
+        weapon = self.WEAPON_NAMES_BY_ID[data.data[1]]['name']
         killer = data.client
         victim = data.target
         if self._punisher_settings['kill_player']:
@@ -200,10 +231,10 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
         _maps = dict()
         for mapname in self.config.sections():
             # 'ziba tower' > 'XP2_Skybar'
-            if mapname.lower() in MAP_ID_BY_NAME:
-                _maps[MAP_ID_BY_NAME.get(mapname.lower())] = ''
+            if mapname.lower() in self.MAP_ID_BY_NAME:
+                _maps[self.MAP_ID_BY_NAME.get(mapname.lower())] = ''
             # 'XP2_Skybar'
-            elif mapname in MAP_NAME_BY_ID:
+            elif mapname in self.MAP_NAME_BY_ID:
                 _maps[mapname] = ''
 
         return _maps
@@ -218,7 +249,7 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
 
         _maps = self._maps_from_fonfig()
         for mapid in _maps:
-            self.debug('Load map configuration for %s', MAP_NAME_BY_ID[mapid])
+            self.debug('Load map configuration for %s', self.MAP_NAME_BY_ID[mapid])
             setmapconfig(mapid)
             if not self._mapconfig_is_valid(mapid):
                 self.error('Configuration for %s are wrong!', mapid)
@@ -227,12 +258,12 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
     def _mapconfig_is_valid(self, mapid):
         # check weapon list
         for weapon in self._mapconfig[mapid].get('weapons'):
-            if weapon not in list(WEAPON_NAMES_BY_ID):
+            if weapon not in list(self.WEAPON_NAMES_BY_ID):
                 self.debug('%s is not a valied weapon')
                 return False
             #check gamemodes
         for gamemode in self._mapconfig[mapid].get('gametype'):
-            if gamemode not in GAME_MODES_BY_MAP_ID[mapid] or gamemode in self._disable_on_gamemode:
+            if gamemode not in self.GAME_MODES_BY_MAP_ID[mapid] or gamemode in self._disable_on_gamemode:
                 self.debug('%s not support %s' % (mapid, gamemode))
                 return False
 
@@ -244,7 +275,7 @@ class Weaponlimiterbf3Plugin(b3.plugin.Plugin):
             weaponlist = self._mapconfig[self.console.game.mapName]['weapons']
 
         for weapon in weaponlist:
-            _weaponlist.append(WEAPON_NAMES_BY_ID[weapon]['name'])
+            _weaponlist.append(self.WEAPON_NAMES_BY_ID[weapon]['name'])
 
         return _weaponlist
 
